eb5f42945b6039de756e852d3c26a8d5d1225c88
Ömer Satık <omer.satik@westerops.com>
2022-10-01 22:23:44 +0300

    pom updated

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,9 +1,6 @@
 import com.intuit.karate.junit5.Karate;
-import cucumber.api.CucumberOptions;
 
 
-@CucumberOptions(plugin = { "pretty", "json:target/cucumber.json" }, features = {
-        "src/test/resources/features/" }, glue = { "classpath:" }, tags = { "@wip" })
 class TestRunner {
     @Karate.Test
     Karate testTags() {
