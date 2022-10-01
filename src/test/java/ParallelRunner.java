2bba74702db734e477d5167c7adb21d6a541fd78
Ömer Satık <omer.satik@westerops.com>
2022-10-01 23:33:41 +0300

    -

--- src/test/java/ParallelRunner.java
+++ src/test/java/ParallelRunner.java
@@ -1,16 +1,9 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
+
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import static org.testng.AssertJUnit.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 
 public class ParallelRunner {
