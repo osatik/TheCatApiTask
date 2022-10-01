07e96d152f5595a1ba1d1704635826dd2cea3b61
Ömer Satık <omer.satik@westerops.com>
2022-10-01 23:44:53 +0300

    -

--- src/test/java/ParallelRunner.java
+++ src/test/java/ParallelRunner.java
@@ -1,17 +1,13 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-
 import org.junit.jupiter.api.Test;
-
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ParallelRunner {
     @Test
     public void testParallel() {
         Results results = Runner.path("classpath:features").tags("~@ignore").parallel(5);
-        assertEquals(results.getErrorMessages(), 0, results.getFailCount());
+        assertTrue(results.getFailCount() == 0, results.getErrorMessages());
     }
-
-
 }
