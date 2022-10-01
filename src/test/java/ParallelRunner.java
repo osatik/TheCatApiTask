c7078683518168a8335ec0ac62a1195d7b34b8ea
Ömer Satık <omer.satik@westerops.com>
2022-10-01 23:30:37 +0300

    pom updated

--- src/test/java/ParallelRunner.java
+++ src/test/java/ParallelRunner.java
@@ -3,30 +3,22 @@ import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.*;
+
 
 public class ParallelRunner {
     @Test
     public void testParallel() {
         Results results = Runner.path("classpath:features").tags("~@ignore").parallel(5);
-        generateReport(results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+        assertEquals(results.getErrorMessages(), 0, results.getFailCount());
     }
 
-    public static void generateReport(String karateOutputPath) {
-        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-        List<String> jsonPaths = new ArrayList(jsonFiles.size());
-        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "TheCatApiTask");
-        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-        reportBuilder.generateReports();
-    }
 
 }
